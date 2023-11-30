3e3fcfa1a90efb3098a1bf04459b807ce34fa5a1
Seungjun Lee <108439947+Leonaxdo@users.noreply.github.com>
2023-11-30 10:44:51 +0900

    Add files via upload

---
+++ lib/domain/perfume/perfume_provider.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:darkchoco/domain/perfume/perfume.dart';
+import 'package:darkchoco/domain/repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final perfumeListProvider = AsyncNotifierProvider<PerfumeListNotifier, PerfumeList>(() => PerfumeListNotifier());
+
+class PerfumeListNotifier extends AsyncNotifier<PerfumeList>{
+
+  void getPerfumeList() async {
+    state = await AsyncValue.guard(() => ref.read(repositoryProvider).getPerfumeList());
+  }
+
+  @override
+  FutureOr<PerfumeList> build() async{
+    // TODO: implement build
+    getPerfumeList();
+    print('error: ${state.error}');
+    return state.value!;
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'domain/perfume/perfume_provider.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: implement build
+
+    final perfumeList = ref.watch(perfumeListProvider);
+
+    return Scaffold(
+      body: perfumeList.when(data: (perfumeList) {
+        return ListView.builder(
+          itemCount: perfumeList.size,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(perfumeList.content[index].name),
+            );
+          },
+        );
+      }, loading: () {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }, error: (error, stackTrace) {
+        return const Center(
+          child: Text('Error'),
+        );
+      }),
+    );
+  }
+}
